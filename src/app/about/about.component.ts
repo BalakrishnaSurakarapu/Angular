bd2f11cfe61b26d71c814e401c8dd1a6fcffc363
BalakrishnaSurakarapu <balugoud508@gmail.com>
2024-08-08 19:23:40 +0530

    add pjt

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  companyRegistrationForm!: FormGroup;
+  emailForm!: FormGroup;
+  genderOptions = ['Male', 'Female', 'Other'];
+
+  createAddress(): FormGroup {
+    return this.formBuilder.group({
+      type: ['', Validators.required],
+      houseNo: ['', Validators.required],
+      street: ['', Validators.required],
+      landmark: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      country: ['', Validators.required],
+      zipCode: ['', Validators.required]
+    });
+  }
+
+  constructor(private formBuilder: FormBuilder) {debugger
+    this.companyRegistrationForm = this.formBuilder.group({
+      companyname:['',Validators.required],
+      companyregistrationno:['',Validators.required],
+     // email: ['', Validators.required, Validators.email],
+      addresses: this.formBuilder.array([this.createAddress()]),
+      emails: this.formBuilder.array([this.createEmail()]),
+      gender: ['', Validators.required],
+      username: ['', Validators.required],
+      dob: ['', Validators.required],
+      lastname: ['', Validators.required],
+      firstname: ['', Validators.required],
+      middlename: [''],
+    });
+
+  }
+  ngOnInit(): void {
+    // this.formBuilder.array([]),
+    //   this.formBuilder.array([this.createAddress()]);
+      
+  }
+  get addresses(): FormArray {
+    return this.companyRegistrationForm.get('addresses') as FormArray;
+  }
+  removeAddress(index: number): void {
+    this.addresses.removeAt(index);
+  }
+  addAddress(): void {debugger
+    this.addresses.push(this.createAddress());
+  }
+
+  get hrDetails(): FormArray {
+    return this.companyRegistrationForm.get('hrDetails') as FormArray;
+  }
+  createEmail(): FormGroup {
+    return this.formBuilder.group({
+     email: ['', [Validators.required, Validators.email]]
+    });
+  }
+  get emails(): FormArray {
+    return this.companyRegistrationForm.get('emails') as FormArray;
+  }
+  addEmail(): void {
+    this.emails.push(this.createEmail());
+  }
+  removeEmail(index: number): void {
+    this.emails.removeAt(index);
+  }
+  onSubmit(): void {debugger
+    console.log(this.companyRegistrationForm.value);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AboutComponent } from './about/about.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule 
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
